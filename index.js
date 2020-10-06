5a3cda9b96ed999a8e2a6402fe58a840eba10c40
Ethan <eleetyson@gmail.com>
2020-10-06 16:01:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// storing a node in the input constant
+// invoking addEventListener on that constant with...
+  // a click as the event to listen for
+  // and a callback function to be executed when the event occurs
 function addingEventListener() {
+  const input = document.getElementById('input');
+  input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });
 }
